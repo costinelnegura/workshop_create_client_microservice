ad8fd5371ca09c0808f8f95aec1c261a6b163569
costinelnegura <costinelnegura@Costinels-MacBook-Pro.local>
2022-07-30 15:29:43 +0100

    create UserRepo interface

---
+++ src/main/java/com/costinel/repo/UserRepo.java
@@ -0,0 +1,7 @@
+package com.costinel.repo;
+
+import com.costinel.workshop_create_client_microservice.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<Client, Long> {
+}
--- src/main/java/com/costinel/workshop_create_client_microservice/controller/WorkshopCreateClientMicroserviceApplicationController.java
+++ src/main/java/com/costinel/workshop_create_client_microservice/controller/WorkshopCreateClientMicroserviceApplicationController.java
@@ -1,9 +1,11 @@
 package com.costinel.workshop_create_client_microservice.controller;
 
+import com.costinel.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WorkshopCreateClientMicroserviceApplicationController {
-
 
 }
