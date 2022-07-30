054b9c4d4630f346ba76abc14b745f1caf7bd73c
costinelnegura <costinelnegura@Costinels-MacBook-Pro.local>
2022-07-30 16:26:03 +0100

    jdbc mysql connection properties

--- src/main/java/com/costinel/workshop_create_client_microservice/controller/WorkshopCreateClientMicroserviceApplicationController.java
+++ src/main/java/com/costinel/workshop_create_client_microservice/controller/WorkshopCreateClientMicroserviceApplicationController.java
@@ -1,11 +1,14 @@
 package com.costinel.workshop_create_client_microservice.controller;
 
-import com.costinel.repo.UserRepo;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WorkshopCreateClientMicroserviceApplicationController {
+
+    @GetMapping(value = "/test")
+    public String getString(){
+        return "string test";
+    }
 
 }
