b2412ae0d90a15474d6c3cdc903f6b506a6f32fd
costinelnegura <costinelnegura@Costinels-MacBook-Pro.local>
2022-07-30 17:50:21 +0100

    CRUD completed

--- src/main/java/com/costinel/workshop_create_client_microservice/controller/WorkshopCreateClientMicroserviceApplicationController.java
+++ src/main/java/com/costinel/workshop_create_client_microservice/controller/WorkshopCreateClientMicroserviceApplicationController.java
@@ -3,8 +3,7 @@ package com.costinel.workshop_create_client_microservice.controller;
 import com.costinel.repo.UserRepo;
 import com.costinel.workshop_create_client_microservice.model.Client;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,14 +13,41 @@ public class WorkshopCreateClientMicroserviceApplicationController {
     @Autowired
     private UserRepo userRepo;
 
-    @GetMapping(value = "/test")
-    public String getString(){
-        return "string test";
-    }
-
-    @GetMapping(value = "/clients")
+    @GetMapping(value = "/getAllClients")
     public List<Client> getAllClients(){
         return userRepo.findAll();
     }
 
+    @GetMapping(value = "/getClient/{id}")
+    public List<Client>  getClient(@PathVariable long id){
+        return userRepo.findById(id).stream().toList();
+    }
+
+    @PostMapping(value = "/saveClient")
+    public String saveClient(@RequestBody Client client){
+        userRepo.save(client);
+        return "Client saved successfully!";
+    }
+
+    @PutMapping(value = "/updateClient/{id}")
+    public String updateClient(@PathVariable long id, @RequestBody Client client){
+        Client clientToUpdate = userRepo.findById(id).get();
+        clientToUpdate.setProNoun(client.getProNoun());
+        clientToUpdate.setFirstName(client.getFirstName());
+        clientToUpdate.setLastName(client.getLastName());
+        clientToUpdate.setHomeAddress(client.getHomeAddress());
+        clientToUpdate.setPostCode(client.getPostCode());
+        clientToUpdate.setMobileNumber(client.getMobileNumber());
+        clientToUpdate.setLandLineNumber(client.getLandLineNumber());
+        clientToUpdate.setWorkNumber(client.getWorkNumber());
+        clientToUpdate.setEmailAddress(client.getEmailAddress());
+        userRepo.save(clientToUpdate);
+        return "Client updated successfully!";
+    }
+
+    @DeleteMapping(value = "deleteClient/{id}")
+    public String deleteClient(@PathVariable long id){
+        userRepo.delete(userRepo.findById(id).get());
+        return "Client deleted successfully!";
+    }
 }
